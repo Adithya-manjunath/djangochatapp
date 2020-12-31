6959b6a49e1134b0524d220809e0f572fecbe5a8
unknown <madithya33@gmail.com>
2020-12-31 23:14:48 +0530

    some stuff

--- mysite/chat/routing.py
+++ mysite/chat/routing.py
@@ -6,10 +6,10 @@ from . import consumers
 
 websocket_urlpatterns = [
     
-    re_path(r'ws/chat/number/(?P<button_name>\w+)/$',consumers.ButtonClick.as_asgi()),
-    re_path(r'ws/chat/coords/(?P<x>\w+)/get/(?P<y>\w+)/get/(?P<sx>\w+)/get/(?P<sy>\w+)/$',consumers.Coordinate.as_asgi()),
+    re_path(r'ws/chat/number/(?P<button_name>\w+)/$',consumers.ButtonClick),
+    re_path(r'ws/chat/coords/(?P<x>\w+)/get/(?P<y>\w+)/get/(?P<sx>\w+)/get/(?P<sy>\w+)/$',consumers.Coordinate),
    
-    re_path(r'ws/chat/(?P<room_name>\w+)/(?P<user_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/(?P<user_name>\w+)/$', consumers.ChatConsumer),
     
 ]
 
